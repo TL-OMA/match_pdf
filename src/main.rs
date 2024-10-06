6f292eb3ef57a6038cb572761a6f6d20f1aedaf6
Travis Lawrenson <travis.lawrenson@gmail.com>
2024-10-06 13:14:34 -0500

    Removed some license command line flag code

--- src/main.rs
+++ src/main.rs
@@ -57,9 +57,6 @@ struct Cli {
     #[arg(short, long)]
     config: Option<PathBuf>,
 
-    /// An optional 'license' flag: Force the prompt for a new license, even if one is stored locally already.
-    #[arg(short, long)]
-    license: bool,
 }
 
 
